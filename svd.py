b64aed15b37ae5c9b9581dcfe6507e75e14b0936
RobertKochanski <kochanskirobert1999@gmail.com>
2022-06-07 22:05:56 +0200

    SVD

--- svd.py
+++ svd.py
@@ -12,50 +12,35 @@ def svd(A):
     U = np.zeros((m, m))
     VT = np.zeros((n, n))
 
-    if m < n:
-        for x in range(m):
-            u.append(A @ V[:, x] / norm(A @ V[:, x]))
+    for x in range(n):
+        u.append(A @ V[:, x] / norm(A @ V[:, x]))
 
-        for i in range(m):
-            U[i] = u[i]
+    for i in range(n):
+        U[i] = u[i]
 
-        U = U.T
+    U = U.T
 
-        for j in range(n):
-            VT[j] = V[:, j]
+    for j in range(n):
+        VT[j] = V[:, j]
 
-        S = np.round(U.T @ A @ V, 10)
-
-    else:
-        for x in range(n):
-            u.append(A @ V[:, x] / norm(A @ V[:, x]))
-
-        for i in range(n):
-            U[i] = u[i]
-
-        U = U.T
-
-        for j in range(n):
-            VT[j] = V[:, j]
-
-        S = np.round(U.T @ A @ V, 10)
+    S = np.round(U.T @ A @ V, 10)
 
     return U, S, VT
 
 
-A = np.array([[1, 1],
-             [1, 3]])
+# A = np.array([[3, 2],
+#              [1, 3]])
 
-# A = np.array([[1, 1, 0],
+# A = np.array([[4, 1, 0],
 #              [1, 3, 1],
-#              [0, 1, 1]])
+#              [3, 1, 5]])
 
 # A = np.array([[1, 1],
 #              [1, 0],
 #              [4, 1]])
 
-# A = np.array([[1, 1, 0],
-#              [1, 5, 1]])
+A = np.array([[1, 1, 0],
+             [1, 5, 1]])
 
 if A.shape[0] < A.shape[1]:
     A = A.T
